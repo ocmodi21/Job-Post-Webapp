1f0258e4fa47c52cef6bdd69739c1baa32519c4e
ocmodi21 <ocmodi2117@gmail.com>
2024-05-07 12:10:52 +0530

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
